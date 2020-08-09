7966b636bef2d3fbe23cbcda64d32138e0106ad2
KvngColl <46813694+KvngColl@users.noreply.github.com>
2020-08-09 20:42:18 +0000

    Update HashTable.py

--- HashTable.py
+++ HashTable.py
@@ -1,47 +1,47 @@
 import random
 import time
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plots
 
 
 class HashTable:
     def _init_(self):
         self.MAX = 509
-        self.arr = [[] for _ in range(self.MAX)]
-        self.tree_arr = [None] * 509
+        self.array = [[] for _ in range(self.MAX)]
+        self.Tree_Array = [None] * 509
 
-    def get_hash(self, key):
+    def H_ash(self, key):
         hash = key
         return hash % self.MAX
 
-    def insertChaining(self, key, val):
-        h = self.get_hash(key)
-        if not (self.arr[h] == "[]"):
-            self.arr[h].append(val)
+    def Insert_Chain(self, key, val):
+        h = self.H_ash(key)
+        if not (self.array[h] == "[]"):
+            self.array[h].append(val)
         else:
-            self.arr[h] = val
+            self.array[h] = val
 
-    def insertTree(self, key, val):
-        h = self.get_hash(key)
-        # print(self.tree_arr[h])
-        if (self.tree_arr[h] == None):
-            self.tree_arr[h] = Tree()
-            self.tree_arr[h].insertT(val)
+    def Insert_Tree(self, key, val):
+        h = self.H_ash(key)
+        # print(self.Tree_Array[h])
+        if (self.Tree_Array[h] == None):
+            self.Tree_Array[h] = Tree()
+            self.Tree_Array[h].insertT(val)
         else:
-            self.tree_arr[h].insertT(val)
+            self.Tree_Array[h].insertT(val)
 
     def display_hash(self):
         size = self.MAX
         for i in range(size):
             print("[", i, "]", end=" ")
-            print(self.arr[i])
+            print(self.array[i])
 
-    def display_hash_Tree(self):
+    def Display_Tree(self):
         size = self.MAX
         for i in range(size):
             print("[", i, "]", end=" ")
-            if self.tree_arr[i] != None:
+            if self.Tree_Array[i] != None:
                 print("[ ", end="")
-                self.tree_arr[i].inorder()
+                self.Tree_Array[i].In_order()
             else:
                 print("[]")
 
@@ -49,34 +49,34 @@ class HashTable:
 class Node:
     def _init_(self, val):
         self.value = val
-        self.leftChild = None
-        self.rightChild = None
+        self.Left_Child = None
+        self.Right_Child = None
 
     def insert(self, data):
         if self.value == data:
             return False
 
         elif self.value > data:
-            if self.leftChild:
-                return self.leftChild.insert(data)
+            if self.Left_Child:
+                return self.Left_Child.insert(data)
             else:
-                self.leftChild = Node(data)
+                self.Left_Child = Node(data)
                 return True
 
         else:
-            if self.rightChild:
-                return self.rightChild.insert(data)
+            if self.Right_Child:
+                return self.Right_Child.insert(data)
             else:
-                self.rightChild = Node(data)
+                self.Right_Child = Node(data)
                 return True
 
-    def inorder(self):
+    def In_order(self):
         if self:
-            if self.leftChild:
-                self.leftChild.inorder()
+            if self.Left_Child:
+                self.Left_Child.In_order()
             print(str(self.value), end=" ")
-            if self.rightChild:
-                self.rightChild.inorder()
+            if self.Right_Child:
+                self.Right_Child.In_order()
 
 
 class Tree:
@@ -90,16 +90,16 @@ class Tree:
             self.root = Node(data)
             return True
 
-    def inorder(self):
+    def In_order(self):
         if self.root is not None:
-            self.root.inorder()
+            self.root.In_order()
             print(']\n', end="")
 
 
-table1 = HashTable()
-table2 = HashTable()
-tree_y1, tree_y2, tree_y3, tree_y4, tree_y5, tree_y6, tree_y7, tree_y8 = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-chain_y1, chain_y2, chain_y3, chain_y4, chain_y5, chain_y6, chain_y7, chain_y8 = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+Table_A = HashTable()
+Table_B = HashTable()
+T_y1, T_y2, T_y3, T_y4, T_y5, T_y6, T_y7, T_y8 = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+C_y1, C_y2, C_y3, C_y4, C_y5, C_y6, C_y7, C_y8 = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
 
 # hash with chain
 print("Hash with chain")
@@ -108,12 +108,12 @@ sum_time = 0
 for current in range(8192):
     count = count + 1
     start_time = time.time()
-    r = int(random.uniform(16385, 65335))
-    midSqSeed = r
-    midSqSeed = midSqSeed * midSqSeed
-    midSqSeed = midSqSeed / 100
-    midSquare = int(midSqSeed % 10000)
-    table2.insertChaining(midSquare, r)
+    rand = int(random.uniform(16384, 65535))
+    CSeed= rand
+    CSeed= CSeed* CSeed
+    CSeed= CSeed/ 100
+    Mid_Square = int(CSeed% 10000)
+    Table_B.Insert_Chain(Mid_Square, rand)
 
     end_time = time.time()
     elapsed_time = end_time - start_time
@@ -121,44 +121,44 @@ for current in range(8192):
     sum_time = sum_time + elapsed_time_milliSeconds
 
     if (current == 1023):
-        chain_y1 = round(sum_time / count, 6)
-        print(chain_y1)
+        C_y1 = round(sum_time / count, 6)
+        print(C_y1)
         sum_time = 0
     if (current == 2047):
-        chain_y2 = round(sum_time / count, 6)
-        print(chain_y2)
+        C_y2 = round(sum_time / count, 6)
+        print(C_y2)
         sum_time = 0
     if (current == 3071):
-        chain_y3 = round(sum_time / count, 6)
-        print(chain_y3)
+        C_y3 = round(sum_time / count, 6)
+        print(C_y3)
         sum_time = 0
     if (current == 4095):
-        chain_y4 = round(sum_time / count, 6)
-        print(chain_y4)
+        C_y4 = round(sum_time / count, 6)
+        print(C_y4)
         sum_time = 0
     if (current == 5119):
-        chain_y5 = round(sum_time / count, 6)
-        print(chain_y5)
+        C_y5 = round(sum_time / count, 6)
+        print(C_y5)
         sum_time = 0
     if (current == 6143):
-        chain_y6 = round(sum_time / count, 6)
-        print(chain_y6)
+        C_y6 = round(sum_time / count, 6)
+        print(C_y6)
         sum_time = 0
     if (current == 7167):
-        chain_y7 = round(sum_time / count, 6)
-        print(chain_y7)
+        C_y7 = round(sum_time / count, 6)
+        print(C_y7)
         sum_time = 0
     if (current == 8191):
-        chain_y8 = round(sum_time / count, 6)
-        print(chain_y8)
+        C_y8 = round(sum_time / count, 6)
+        print(C_y8)
         sum_time = 0
 
-# table2.display_hash()
+# Table_B.display_hash()
 
 
 print("\n")
 print(
-    "**********************************************")
+    "<------------------------------------------------>")
 print("\n")
 print("Hash with Binary Tree")
 count = 0
@@ -166,59 +166,59 @@ sum_time = 0
 for current in range(8192):
     count = count + 1
     start_time = time.time()
-    r = int(random.uniform(16385, 65335))
-    midSqSeed = r
-    midSqSeed = midSqSeed * midSqSeed
-    midSqSeed = midSqSeed / 100
-    midSquare = int(midSqSeed % 10000)
-    table1.insertTree(midSquare, r)
+    rand = int(random.uniform(16385, 65335))
+    CSeed= rand
+    CSeed= CSeed* midSqSeed
+    CSeed= CSeed/ 100
+    Mid_Square = int(CSeed% 10000)
+    Table_A.Insert_Tree(Mid_Square, rand)
     end_time = time.time()
     elapsed_time = end_time - start_time
     elapsed_time_milliSeconds = elapsed_time * 1000
     sum_time = sum_time + elapsed_time_milliSeconds
 
     if (current == 1023):
-        tree_y1 = round(sum_time / count, 6)
-        print(tree_y1)
+        T_y1 = round(sum_time / count, 6)
+        print(T_y1)
         sum_time = 0
     if (current == 2047):
-        tree_y2 = round(sum_time / count, 6)
-        print(tree_y2)
+        T_y2 = round(sum_time / count, 6)
+        print(T_y2)
         sum_time = 0
     if (current == 3071):
-        tree_y3 = round(sum_time / count, 6)
-        print(tree_y3)
+        T_y3 = round(sum_time / count, 6)
+        print(T_y3)
         sum_time = 0
     if (current == 4095):
-        tree_y4 = round(sum_time / count, 6)
-        print(tree_y4)
+        T_y4 = round(sum_time / count, 6)
+        print(T_y4)
         sum_time = 0
     if (current == 5119):
-        tree_y5 = round(sum_time / count, 6)
-        print(tree_y5)
+        T_y5 = round(sum_time / count, 6)
+        print(T_y5)
         sum_time = 0
     if (current == 6143):
-        tree_y6 = round(sum_time / count, 6)
-        print(tree_y6)
+        T_y6 = round(sum_time / count, 6)
+        print(T_y6)
         sum_time = 0
     if (current == 7167):
-        tree_y7 = round(sum_time / count, 6)
-        print(tree_y7)
+        T_y7 = round(sum_time / count, 6)
+        print(T_y7)
         sum_time = 0
     if (current == 8191):
-        tree_y8 = round(sum_time / count, 6)
-        print(tree_y8)
+        T_y8 = round(sum_time / count, 6)
+        print(T_y8)
         sum_time = 0
 
-# table1.display_hash_Tree()
+# Table_A.Display_Tree()
 
 x = ["1024", "2048", "3072", "4096", "5120", "6144", "7168", "8192"]
-chain = [chain_y1, chain_y2, chain_y3, chain_y4, chain_y5, chain_y6, chain_y7, chain_y8]
+chain = [C_y1, C_y2, C_y3, C_y4, C_y5, C_y6, C_y7, C_y8]
 print(chain)
-tree = [tree_y1, tree_y2, tree_y3, tree_y4, tree_y5, tree_y6, tree_y7, tree_y8]
+tree = [T_y1, T_y2, T_y3, T_y4, T_y5, T_y6, T_y7, T_y8]
 print(tree)
-plt.plot(x, tree)
-plt.plot(x, chain)
-plt.xlabels("Intervals")
-plt.title("Runtime Comparison")
-plt.show()
+plots.plot(x, tree)
+plots.plot(x, chain)
+plots.xlabels("Intervals")
+plots.title("Runtime Comparison:")
+plots.show()
